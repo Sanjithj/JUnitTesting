02c1d00ce2d6d2d189f491563d62af91bf8fdeb3
Sanjithj <sanjithj591997@gmail.com>
2022-06-29 16:49:43 +0530

    Added: Last Name

--- RegistrationTesting.java
+++ RegistrationTesting.java
@@ -6,31 +6,30 @@ import java.util.regex.Pattern;
 
 public class RegistrationTesting {
 	
-	public static void checkValidFirstName(String firstName) {
-
-        boolean isFirstName;
-        String firstNameRegex = "[A-Z]{1}[a-z]{2,}";
-        Pattern patternObject = Pattern.compile(firstNameRegex);
-        
-        if (firstName == null) {
-            isFirstName = false;
-        }
-        Matcher matcherObject = patternObject.matcher(firstName);
-        isFirstName =  matcherObject.matches();
-
-        if(isFirstName)
-            System.out.println(firstName+  " Valid First Name");
-        else
-            System.out.println(firstName+  " Invalid First Name");
-    }  
-	
-	public static void main(String[] args) {
+	public static void checkValidLastName(String lastName) {
 		
-	 Scanner scannerObject = new Scanner(System.in);
+	    boolean isLastName;
+	    String lastNameRegex = "[A-Z]{1}[a-z]{2,}";
+	    Pattern patternObject = Pattern.compile(lastNameRegex);
+	    if (lastName == null) {
+	        isLastName = false;
+	    }
+	    Matcher matcherObject = patternObject.matcher(lastName);
+	    isLastName = matcherObject.matches();
+	
+	    if (isLastName)
+	        System.out.println(lastName + " Valid Last Name");
+	    else
+	        System.out.println(lastName + "  Invalid Last Name");
+	}
+	 public static void main(String[] args) {
+		
+	Scanner scannerObject = new Scanner(System.in);
 
-     System.out.println("Enter the First Name");
-     String firstName = scannerObject.next();
-     checkValidFirstName(firstName);
-	}  
+    System.out.println("Enter Your Last Name");
+    String lastName = scannerObject.next();
+    checkValidLastName(lastName);
 
+	 }
 }
+
